fcbf1ebc70c9a76fe4840c8a1109c44636a3ef1e
nimrodsilberfeld <nimrod513@gmail.com>
2021-11-12 17:05:36 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { deleteImage, getImagesData, uploadImage } from "./server/images";
 
 function App() {
+  const [images, setImgaes] = useState([])
+  //const URL = "https://nim-photos-storage.s3.eu-west-1.amazonaws.com/"
+  const URL = "http://localhost:3030/get-image"
+
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    const image = e.target.children[0].files[0];
+    const formData = new FormData()
+    formData.append("image", image)
+
+    uploadImage(formData)
+      .then(res => {
+        console.log(res)
+        return getImagesData()
+      })
+      .then(newImages => {
+        setImgaes(newImages)
+      })
+  }
+
+  const onClickDelete = (id, key) => {
+    deleteImage(id, key)
+      .then(() => {
+        return getImagesData()
+      })
+      .then(newImages => {
+        setImgaes(newImages)
+      })
+  }
+
+
+  useEffect(() => {
+    getImagesData()
+      .then((newImages) => {
+        setImgaes(newImages)
+
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Images app</h1>
+      <form onSubmit={onSubmitForm}>
+        <input type="file" name="image" />
+        <button type="submit">Submit</button>
+      </form>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
+        {
+          images.map((image) => (
+            <div key={image._id} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <h3>{image.originalName || "default name"}</h3>
+              <img
+                style={{ width: 250, height: 250 }}
+                src={URL + `?key=${image.key}&name=${image.originalName}`}
+                alt={image.originalName} />
+              <button onClick={() => {
+                onClickDelete(image._id, image.key)
+              }}>
+                Delete {image.originalName}
+              </button>
+              <hr style={{
+                border: 0, display: "block", width: "50vw", backgroundColor: "black", height: "0.5px"
+              }} />
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
